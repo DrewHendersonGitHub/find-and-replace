250a37a18754741821bc2f04ca8ad85bcd2ea0eb
Drew Henderson <drewhendersonemail@gmail.com>
2021-12-08 13:05:43 -0800

    added ability to find partial words
    Co-authored-by: Christopher Neal <christopher.m.neal@gmail.com>

--- lib/title_case.rb
+++ lib/title_case.rb
@@ -1,15 +1,28 @@
 class Text
   def initialize(text)
     @text = text
-    @words = []
   end
   def find_and_replace(find, replace)
-    @words = @text.split(" ")
-    @words.each_with_index do |word, i|
-      if word == find
-        @words[i] = replace
+    words = @text.split("")
+    find.split("")
+    words.each_with_index do |word, i|
+      if i < words.length - find.length + 1
+        counter = 0
+        check = true
+        while counter < find.length && check == true
+          if words[i + counter] != find[counter]
+            check = false
+          end
+          counter+=1
+        end
+        if check
+          for a in 1..find.length-1 do
+            words[i + a] = ''
+          end
+          words[i] = replace
+        end
       end
     end
-    @words.join(" ")
+    words.join("")
   end
 end
